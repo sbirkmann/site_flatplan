bb4b7718b423e83b5003e47df5b2b745e623277f
John Doe <johndoe@example.com>
2026-04-05 22:05:11 +0200

    wsdw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://flatplan.de'),
   title: "flatplan.de | 3D Wohnungsfinder & Digitales Immobilienmarketing",
   description: "Entdecken Sie den interaktiven 3D Wohnungsfinder für Ihr Neubauprojekt. Optimiert für Bauträger und Vermarkter.",
+  verification: {
+    google: "bpJ2DiIH1PxIhuL_ZejfuBQMhhCfFQlal6QK9UD2ZQw",
+  },
 };
 
 export default function RootLayout({
